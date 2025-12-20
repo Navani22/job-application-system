184c12cae349293d594a5ee70cbef0df7ed4c43c
Navani22 <swaminavani@gmail.com>
2025-12-20 12:28:17 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/navani/jobapp/JobApplicationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navani.jobapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

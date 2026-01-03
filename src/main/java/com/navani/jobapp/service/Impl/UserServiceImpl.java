e0ffa5c9b554b59ba0c7e647cef2c6f7573eeca3
Navani22 <swaminavani@gmail.com>
2026-01-03 12:47:32 +0530

    Added UserService and implementation

---
+++ src/main/java/com/navani/jobapp/service/Impl/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.navani.jobapp.service.impl;
+
+import com.navani.jobapp.entity.User;
+import com.navani.jobapp.repository.UserRepository;
+import com.navani.jobapp.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User createUser(User user) {
+        user.setCreatedAt(LocalDateTime.now());
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElse(null);
+    }
+}
---
+++ src/main/java/com/navani/jobapp/service/UserService.java
@@ -0,0 +1,17 @@
+package com.navani.jobapp.service;
+
+import com.navani.jobapp.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+
+    User createUser(User user);
+
+    Optional<User> getUserById(Long id);
+
+    List<User> getAllUsers();
+
+    User getUserByEmail(String email);
+}
